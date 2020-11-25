8112c2b3b75d1bcf7c2b4588c7befbb8788f5886
bidisha12 <bidishapaul237@gmail.com>
2020-11-25 19:05:53 +0530

    update
    Signed-off-by: bidisha12 <bidishapaul237@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,33 @@
-import React, {useState} from "react";
-import ListItem from "./ListItem";
+import React, { useState } from "react";
 import "./../styles/App.css";
-
+import ListItem from './ListItem';
 function App() {
 	const [items, setItems] = useState([]);
 	const [newItem, setNewItem] = useState("");
-	const addItem = () => {
-		let newItemList=[...items].concat([newItem]);
-		 setItems([newItemList]);
-		//items.push = (newItem);
-		//setItems([items]);
-		
+	const addItem = () =>{
+		items.push(newItem);
+		setItems([...items]);
 		setNewItem("");
 	};
-	const newItemChanged = (evt) => {
+	const setNewItemChanged = (evt) => {
 		setNewItem(evt.target.value);
 	};
-	const deleteHandeler=(itemidx)=>{
-		items.slice(itemidx,1);
-		setItems([items]);
-	}
-	const editHandeler=(editedValue,itmIdx)=>{
-		items[itmIdx]=editedValue;
+	const deleteHandler = (itemIdx) => {
+		items.splice(itemIdx,1);
 		setItems([...items]);
-	}
+	};
+	const editHandler = (editedValue, itemIdx) => {
+		items[itemIdx] = editedValue;
+		setItems([...items]);
+	} 
 	return (
-		<div id="main">
-			<textarea id="task" 
-			onChange={newItemChanged} 
-			placeholder="New Item" 
-			value={newItem}
-			></textarea>
-			<button id="btn" onClick={addItem} disabled={(newItem.trim().length)===0}>Add Item</button>
-			{items.map((item, idx) => (
-				<ListItem item={item} key={`${item}_${idx}`} idx={idx} editHandeler={editHandeler} deleteHandeler={deleteHandeler} />
-			))}
-		</div>
+	<div id="main">
+	<textarea id="task" onChange={setNewItemChanged} placeholder="New Item" value={newItem}></textarea>
+	<button id="btn" onClick={addItem} disabled={newItem.trim().length===0}>Add Item</button>
+	{items.map((item,idx) =>(
+		<ListItem item={item} key={`${item}_${idx}`} idx={idx} editHandler={editHandler} deleteHandler = {deleteHandler}/>
+	))}
+	</div>
 	);
 }
-
-
 export default App;
--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,31 +1,32 @@
 import React, { useState } from "react";
 import "./../styles/App.css";
-
 function ListItem(props) {
     const [editedItem, setEditedItem] = useState(props.item);
-    const [editMode, setEditMode] = useState(false);
+    const [editmode,setEditMode] = useState(false);
     const editedItemChanged = (evt) => {
-		setEditedItem(evt.target.value);
+        setEditedItem(evt.target.value);
     };
-    const saveEditedItem=()=>{
-        props.editHandeler(editedItem,props.idx);
+    const saveEditedItem = () => {
+        props.editHandler(editedItem,props.idx);
         setEditMode(false);
-    }
-    return (
-        <div className="list">
-            {editMode ?
-                <>
-                    <textarea className="editTask" placeholder="Edit Task" value={editedItem}></textarea>
-                    <button className="saveTask"  onClick={saveEditedItem} disabled={editedItem.trim().length === 0}>Save Task</button>
-                </> :
-                <>
-                    {props.item}
-                    <button className="edit" onClick={()=> setEditMode(true)} onChange={editedItemChanged}>Edit</button>
-                    <button className="delete" onClick={() => props.deleteHandeler(props.idx)}>Delete</button>
-                </>}
-        </div>
-    );
-
+    };
+	return (
+	<div className="list">
+        {editmode ? (
+            <>
+            <textarea className="editTask" onChange={editedItemChanged} placeholder="EditTask" value={editedItem}></textarea>
+        <button className="saveTask" onClick={saveEditedItem} disabled={editedItem.trim().length===0}>save Task</button>
+        </>
+        ):(
+            <>
+            {props.item}
+            <button className="edit" onClick = {() => setEditMode(true)}>edit</button>
+            <button className="delete" onClick={() => props.deleteHandler(props.idx)}>Delete</button>
+            </>
+        )}
+	</div>
+	);
 }
+
 
 export default ListItem;
